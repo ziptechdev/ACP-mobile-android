b5d9447fa2c73f567f4e9f588c3d2189f7749acc
Alem Luckin <aluckin2@etf.unsa.ba>
2022-09-29 23:09:27 +0200

    Finising splash fragment

--- app/src/main/java/com/acpmobile/ui/activity/MainActivity.kt
+++ app/src/main/java/com/acpmobile/ui/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.acpmobile.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.acpmobile.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,7 +11,13 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Hide status bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_main)
+
 
     }
 }
