7983ebdebe73969cf3dd7d0f2314ffa86602a3c6
Alem Luckin <aluckin2@etf.unsa.ba>
2022-10-06 23:47:48 +0200

    finish Eligibility Name fragment xml

--- app/src/main/java/com/acpmobile/ui/fragments/Account/EligibilityNameFragment.kt
+++ app/src/main/java/com/acpmobile/ui/fragments/Account/EligibilityNameFragment.kt
@@ -8,13 +8,15 @@ import android.view.ViewGroup
 import com.acpmobile.R
 
 
-class CheckEligibilityOrCreateAccount : Fragment() {
+class EligibilityNameFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_check_eligibility_or_create_account, container, false)
+        return inflater.inflate(R.layout.fragment_eligibility_name, container, false)
     }
+
+
 }
