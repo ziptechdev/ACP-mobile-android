b09327196575fe2abfa7589da03dc28eb27dd3fc
Alem Luckin <aluckin2@etf.unsa.ba>
2022-10-09 18:05:41 +0200

    finish Eligibility Address screen

---
+++ app/src/main/java/com/acpmobile/ui/fragments/Account/EligibilityAddressFragment.kt
@@ -0,0 +1,23 @@
+package com.acpmobile.ui.fragments.Account
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.acpmobile.R
+
+
+class EligibilityAddressFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_eligibility_address, container, false)
+        return view
+    }
+
+}
