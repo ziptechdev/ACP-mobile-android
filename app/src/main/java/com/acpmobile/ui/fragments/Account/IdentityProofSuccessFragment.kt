535c29529ce9ac39f3941bd54846943d93dd0198
Alem Luckin <aluckin2@etf.unsa.ba>
2022-10-18 18:36:34 +0200

    add last screen in identity proof proces and update a identity proof fragment to changing containers views

--- app/src/main/java/com/acpmobile/ui/fragments/Account/IdentityProofSuccessFragment.kt
+++ app/src/main/java/com/acpmobile/ui/fragments/Account/IdentityProofSuccessFragment.kt
@@ -7,25 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import com.acpmobile.R
-import com.acpmobile.databinding.FragmentIdentityProofBinding
+import com.acpmobile.databinding.FragmentIdentityProofSuccessBinding
 
-
-class IdentityProofFragment : Fragment() {
-    private var _binding: FragmentIdentityProofBinding? = null
+class IdentityProofSuccessFragment : Fragment() {
+    private var _binding: FragmentIdentityProofSuccessBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentIdentityProofBinding.inflate(inflater, container, false)
+        _binding = FragmentIdentityProofSuccessBinding.inflate(inflater, container, false)
         val view = binding.root
         binding.containerViewPersonalIdentityBankInfo.tvIdentityProof.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
         binding.containerViewPersonalIdentityBankInfo.tvIdentityProof.setBackgroundResource(R.drawable.round_element_6)
-        binding.containerCircleBar.ivCircle1.setBackgroundResource(R.drawable.circle_element_blue)
+        binding.containerCircleBar.ivCircle4.setBackgroundResource(R.drawable.circle_element_blue)
         return view
-    }
-
-
+        }
 }
