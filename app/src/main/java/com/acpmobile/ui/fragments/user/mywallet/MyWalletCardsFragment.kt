970ef39e8dd2dc5e32b63df71b90388e5bce982a
Muamer Beginovic <muamer.beginovic@levi9.com>
2022-10-30 14:31:51 +0100

    renamed mywallet screen

--- app/src/main/java/com/acpmobile/ui/fragments/user/mywallet/MyWalletCardsFragment.kt
+++ app/src/main/java/com/acpmobile/ui/fragments/user/mywallet/MyWalletCardsFragment.kt
@@ -6,20 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.acpmobile.databinding.FragmentMyWalletBinding
+import com.acpmobile.databinding.FragmentMyWalletCardsBinding
 import com.acpmobile.ui.fragments.user.mywallet.adapter.CreditCardsAdapter
 import com.acpmobile.ui.fragments.user.mywallet.adapter.FundsAdapter
 
-class MyWalletFragment : Fragment() {
+class MyWalletCardsFragment : Fragment() {
 
-    private var _binding: FragmentMyWalletBinding? = null
+    private var _binding: FragmentMyWalletCardsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMyWalletBinding.inflate(inflater, container, false)
+        _binding = FragmentMyWalletCardsBinding.inflate(inflater, container, false)
 
         binding.rvCreditCards.apply {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
