3c8c6ca0628790a93359edaab1505633aa2c47d5
Milan Tukic <m.tukic@levi9.com>
2022-12-30 01:22:13 +0100

    Updated Take a photo

--- app/src/main/java/com/acpmobile/ui/fragments/account/RegistrationScanIDFragment.kt
+++ app/src/main/java/com/acpmobile/ui/fragments/account/RegistrationScanIDFragment.kt
@@ -1,11 +1,22 @@
 package com.acpmobile.ui.fragments.account
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
 import com.acpmobile.R
 import com.acpmobile.databinding.FragmentIdentityProofBinding
 import com.acpmobile.databinding.FragmentPersonalInfoBinding
@@ -21,6 +32,9 @@ class RegistrationScanIDFragment : Fragment() {
 
     private var _binding: FragmentScanIdBinding? = null
     private val binding get() = _binding!!
+    private val CAMERA_PERMISSION_CODE = 1000
+    private val IMAGE_CAPTURE_CODE = 1001
+    private var imageUri: Uri? = null
 
     @Inject
     lateinit var navigation: Navigation
@@ -38,9 +52,76 @@ class RegistrationScanIDFragment : Fragment() {
         binding.containerCircleBar.ivCircle2.setBackgroundResource(R.drawable.circle_element_blue)
 
         binding.btnScanFront.setOnClickListener {
-            navigation.openTakeSelfieFragment()
+//            navigation.openTakeSelfieFragment()
+//            requestCameraPermission()
+
+            // Request permission
+            val permissionGranted = requestCameraPermission()
+            if (permissionGranted) {
+                // Open the camera interface
+                openCameraInterface()
+            }
         }
         return view
     }
 
+    private fun requestCameraPermission(): Boolean {
+        var permissionGranted = false
+// If system os is Marshmallow or Above, we need to request runtime permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val cameraPermissionNotGranted = checkSelfPermission(activity as Context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
+            if (cameraPermissionNotGranted){
+                val permission = arrayOf(Manifest.permission.CAMERA)
+                // Display permission dialog
+                requestPermissions(permission, CAMERA_PERMISSION_CODE)
+            }
+            else{
+                // Permission already granted
+                permissionGranted = true
+            }
+        }
+        else{
+            // Android version earlier than M -&gt; no need to request permission
+            permissionGranted = true
+        }
+        return permissionGranted
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode === CAMERA_PERMISSION_CODE) {
+            if (grantResults.size === 1 && grantResults[0] ==    PackageManager.PERMISSION_GRANTED){
+                // Permission was granted
+                openCameraInterface()
+            }
+            else{
+                // Permission was denied
+                Toast.makeText(context, context?.resources?.getString(R.string.camera_permission_denied), Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun openCameraInterface() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, context?.resources?.getString(R.string.take_a_picture))
+//        values.put(MediaStore.Images.Media.DESCRIPTION, R.string.take_picture_description)
+        imageUri = activity?.contentResolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+// Create camera intent
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+// Launch intent
+        startActivityForResult(intent, IMAGE_CAPTURE_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+// Callback from camera intent
+        if (resultCode == Activity.RESULT_OK){
+            // Set image captured to image view
+            binding.ivPlaceholder.setImageURI(imageUri)
+        }
+        else {
+            // Failed to take picture
+            Toast.makeText(context, context?.resources?.getString(R.string.failed_to_take_picture),  Toast.LENGTH_LONG).show()
+        }
+    }
 }
